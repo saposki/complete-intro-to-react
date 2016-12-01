35637f32eeffb7d1dc02d24d6f95eac9d273b8d7
saposki <saposki@gmail.com>
2016-12-01 08:52:14 -0800

    initial commit

---
+++ ClientApp.js
@@ -0,0 +1,31 @@
+/* global React ReactDOM */
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render: function () {
+    return (
+        div(null,
+          h1({style: {color: this.props.color}}, this.props.title)
+        )
+    )
+  }
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+
+var MyFirstComponent = React.createClass({
+  render: function () {
+    return (
+
+    div(null,
+          MyTitleFact({title: 'Props are great!', color: 'rebeccapurple'}),
+          MyTitleFact({title: 'Use props everywhere!', color: 'mediumaquarmarine'}),
+          MyTitleFact({title: 'Props are the best!', color: 'peru'})
+        )
+    )
+  }
+})
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
