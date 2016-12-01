2bf5676a6c3b1627c9f67661ae42215bbec0fb62
saposki <saposki@gmail.com>
2016-12-01 09:45:02 -0800

    configuration

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,17 +1,8 @@
-/* global React ReactDOM */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyTitle from './MyTitle'
 
 var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-  render: function () {
-    return (
-        div(null,
-          h1({style: {color: this.props.color}}, this.props.title)
-        )
-    )
-  }
-})
 
 var MyTitleFact = React.createFactory(MyTitle)
 
